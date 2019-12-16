c19cb5246c582c5d5534bdbd2cc634cd7697c17a
Wellington Viveiro <wellyviveiro@gmail.com>
2019-12-16 12:04:00 +1100

    First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Fix Websocket protocol on create-react-app 3.3.0
+
+Run on the project's folder
+
+``
+npx fix-cra-300
+``
---
+++ bin/fixcra330.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+
+console.log('Fox React Dev Util');
+
+const currentPath = process.cwd();
+const reactDevUtilsPath = path.join(currentPath, 'node_modules', 'react-dev-utils', 'webpackHotDevClient.js');
+
+if (!fs.existsSync(reactDevUtilsPath)) {
+    console.log('React Dev Utils not installed in current project.');
+    process.exit(1);
+}
+
+
+let content = fs.readFileSync(reactDevUtilsPath, {encoding: 'utf-8'});
+const currentWS = `var connection = new WebSocket(`;
+
+const posWebSocket = content.indexOf(currentWS);
+
+
+if (posWebSocket === -1) {
+    console.log('Websocket not found');
+    process.exit(1);
+}
+
+const prefix = content.substr(0, posWebSocket);
+content = content.substr(posWebSocket);
+content = content.replace("protocol: 'ws',", "protocol: window.location.protocol === 'https:' ? 'wss' : 'ws',");
+content = prefix + content;
+
+fs.writeFileSync(reactDevUtilsPath, content);
+
+console.log('Done!');
+process.exit(1);
